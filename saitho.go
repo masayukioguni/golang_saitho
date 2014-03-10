3be2b16354ba64984f88d2984af71487050a5e8b
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-03-11 00:17:25 +0900

    fix

--- saitho.go
+++ saitho.go
@@ -1,7 +1,14 @@
 // Package newmath is a trivial example package.
 package saitho
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 // Sqrt returns an approximation to the square root of x.
 func Say() string {
+	//says := []string{"a", "b"}
+	fmt.Println(rand.Int31n(100))
 	return "saitho"
 }
